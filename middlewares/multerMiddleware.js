de3e75193f3567dad109837e7b02682d95691d73
abhishekbhattarai0 <abhishekbhattarai831@gmail.com>
2024-09-18 19:43:36 +0545

    fixes directory issue

--- middlewares/multerMiddleware.js
+++ middlewares/multerMiddleware.js
@@ -1,4 +1,5 @@
 import multer from "multer";
+import fs from 'fs'
 
 
 // export const upload = multer({dest:"uploads/profiles/"})
@@ -10,9 +11,16 @@ import multer from "multer";
 const date = Date.now()
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads/profiles/')
-    },
+  destination: function (req, file, cb) {
+    const dir = './uploads/profiles/';
+
+    // Create the directory if it doesn't exist
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true }); // Ensures parent directories are created if they don't exist
+    }
+
+    cb(null, dir); // Proceed with saving to the directory
+  },
     filename: function (req, file, cb) {
       cb(null,date+ file.originalname)
     }
